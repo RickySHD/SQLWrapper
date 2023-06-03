8719a6743bb2b7c3d0f824876d2d3117fd9f6e44
RickySHD <riccio.12.rt@gmail.com>
2023-06-03 02:01:36 +0200

    new: added SQLRow class

---
+++ src/main/java/dev/rickyshd/sqlwrapper/SQLRow.java
@@ -0,0 +1,53 @@
+package dev.rickyshd.sqlwrapper;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public interface SQLRow {
+
+    /**
+     * Create a new {@link SQLRow} from a {@link Map} of {@link String} in {@link Object}.
+     * @param content The Map containing labels and values.
+     * @since 1.2.0
+     * @return A new {@link SQLRow} instance.
+     */
+    static @NotNull SQLRow from(Map<String, Object> content) {
+        return new SQLRowImpl(content);
+    }
+
+    /**
+     * Create an empty {@link SQLRow}.
+     * @return A new empty {@link SQLRow}.
+     * @since 1.2.0
+     */
+    static @NotNull SQLRow empty() {
+        return new SQLRowImpl(new HashMap<>());
+    }
+
+    /**
+     * @return True if not empty, false otherise.
+     * @since 1.2.0
+     */
+    boolean isNotEmpty();
+
+    /**
+     * Get a set of all the labels stored in this {@link SQLRow} instance. From
+     * a Map perspective it's identical to {@link Map#keySet()}
+     * @return A {@link Set} of Strings containing all the labels.
+     * @since 1.2.0
+     */
+    Set<String> getLabels();
+
+    /**
+     * Get a value from the row using a column label.
+     * @param columnLabel The label of the column to get the value from.
+     * @return {@code null} if the columnLabel is not present, the Object corresponding
+     * to the label otherwise.
+     * @since 1.2.0
+     */
+    Object getValue(String columnLabel);
+}
+
---
+++ src/main/java/dev/rickyshd/sqlwrapper/SQLRowImpl.java
@@ -0,0 +1,29 @@
+package dev.rickyshd.sqlwrapper;
+
+import java.util.Map;
+import java.util.Set;
+
+final class SQLRowImpl implements SQLRow {
+    private final Map<String, Object> content;
+
+    public SQLRowImpl(Map<String, Object> _content) {
+        content = _content;
+    }
+
+    @Override
+    public boolean isNotEmpty() {
+        return !content.isEmpty();
+    }
+
+    @Override
+    public Set<String> getLabels() {
+        return content.keySet();
+    }
+
+    @Override
+    public Object getValue(String columnLabel) {
+        if (!content.containsKey(columnLabel)) return null;
+
+        return content.get(columnLabel);
+    }
+}
